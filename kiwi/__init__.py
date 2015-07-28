53962f564085b45fbbd8e6cb8755849ac2aeb4d4
zhangwenbo <zhangwenbo@papayamobile.com>
2015-07-28 16:24:51 +0800

    0.0.2

--- kiwi/__init__.py
+++ kiwi/__init__.py
@@ -2,7 +2,7 @@
 
 from builtins import map
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from .table import *
 from .field import *
--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import os
+import re
+import codecs
 from setuptools import setup
 
-version = '0.0.1'
+ROOT = os.path.dirname(__file__)
+VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+
+with codecs.open(
+        os.path.join(ROOT, 'kiwi', '__init__.py'), 'r', 'utf-8') as init:
+    try:
+        version = VERSION_RE.search(init.read()).group(1)
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
+
 
 setup(name='kiwi',
       version=version,
